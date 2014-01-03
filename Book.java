1e9fac0c2654856e7fae70078ef93dc97561e516
Zachary Donato <zdonato@stevens.edu>
2014-01-02 21:43:07 -0500

    Added Book and Library classes

---
+++ Book.java
@@ -0,0 +1,30 @@
+public class Book{ 
+	String title; 
+	boolean borrowed; 
+	
+	public Book(String bookTitle){ 
+		title = bookTitle; 
+	} // End of Book constructor. 
+	
+	public void Borrowed(){ 
+		borrowed = true; 	
+	} // End of Borrowed(). 
+	
+	public void Returned(){ 
+		borrowed = false; 	
+	} // End of Returned(). 
+	
+	public boolean isBorrowed(){
+		// Returns true if the book is borrowed, false otherwise. 
+		if (borrowed == true){
+			return true; 
+		} else {
+			return false; 
+		}
+	} // End of isBorrowed(). 
+	
+	public String getTitle(){ 
+		return title; 
+	} // End of getTitle().
+	
+}
